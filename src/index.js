d610a781063c8b4992216e91b03f02a05e8c4134
Akemi-Takahashi660 <93993555+Akemi-Takahashi660@users.noreply.github.com>
2022-06-06 22:52:27 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+  alert(inputText);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
